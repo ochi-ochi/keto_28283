d0ac59d1aa70987f38f738256373afe23becc245
ochi-ochi <pgame.pgame@gmail.com>
2020-09-07 19:04:15 +0900

    ユーザー登録
    新規ユーザー登録機能を実施

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,20 @@
 class ArticlesController < ApplicationController
+    before_action :move_to_index, except: [:index]
+
     def index
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def show
+    end
+
+    private
+    def move_to_index
+        unless user_signed_in?
+          redirect_to action: :index
+        end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_032821) do
+ActiveRecord::Schema.define(version: 2020_09_07_095617) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,8 +33,16 @@ ActiveRecord::Schema.define(version: 2020_09_05_032821) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "protein"
+    t.string "fat"
+    t.string "carbo"
+    t.text "introduction", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -42,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_032821) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
