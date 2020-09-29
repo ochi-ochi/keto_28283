915a22429be3192ffd5f1c13a22b0d9b0cea3ded
ochi-ochi <pgame.pgame@gmail.com>
2020-09-29 18:41:17 +0900

    reaction:view,controller

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,6 +1,6 @@
 class ReactionsController < ApplicationController
     def create
-        reaction = Reaction.find_or_initialize_by(user_id: params[:user_id], from_user: current_user.id, article_id: params[article_id])
+        reaction = Reaction.find_or_initialize_by(user_id: params[:user_id], article_id: params[:article_id])
         reaction.update(
           status: params[:reaction]
         )
