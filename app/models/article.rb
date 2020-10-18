3d6e772ac6b230857c065a6eca3addf331233059
ochi-ochi <pgame.pgame@gmail.com>
2020-10-18 22:30:39 +0900

    devise修正
    バリデーション修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
     has_one_attached :image
     belongs_to :user
-    has_many :reaction
+    has_many :reactions
 
     validates :introduction, presence: true
 end
