0cea283fb37aa6b10d5380ad2906d143db0afa5c
ochi-ochi <pgame.pgame@gmail.com>
2020-10-21 18:39:26 +0900

    devise
    ぼっち演算子を使用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
         @myarticle = Article.includes(:user)
-        @name = current_user.name
+        @name = current_user&.name
     end
 end
